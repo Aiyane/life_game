2da85d0a3e752ab67380ecae93afd20504f2f6dd
aiyane <2310091880qq@gmail.com>
2018-09-08 01:51:38 +0800

    修改例子

--- example.py
+++ example.py
@@ -1,28 +1,27 @@
-from life_game import Control, Mapping
+from life_game import Control
 import settings
 
 
 class MyControl(Control):
-    def init_mapping(self):
-        # 重载初始化地图, 去除边框
-        self.game.cell_color = "red"
-        self.game.init_mapping()
+    # 初始化配置
+    cell_color = "red"
+    sleep_time = 100
 
     def sleep(self):
         if self.paint_nums % 5 == 0:
             return 1000
-        return self.mapping.sleep
+        return self.game.sleep_time
 
     def before_paint(self):
         # 每隔5代改变一次颜色
-        self.mapping.generate_next()
-        if self.paint_nums % 5 == 0:
+        self.parent.before_paint()
+        if self.paint_nums > 0 and self.paint_nums % 5 == 0:
             self.game.cell_color = "blue" if self.game.cell_color == "red" else "red"
             for cell in self.get_cells():
                 if cell.lived and cell.shape_obj:
-                    self.cv.itemconfig(cell.shape_obj, 
-                                       fill=self.game.cell_color, 
-                                       outline=self.game.cell_color)
+                    self.canvas.itemconfig(cell.shape_obj,
+                                           fill=self.game.cell_color,
+                                           outline=self.game.cell_color)
 
 
 if __name__ == '__main__':
