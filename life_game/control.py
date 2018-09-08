ddc8892a1196f28daf8a68fd61eb13ddb6f2ecc5
aiyane <2310091880qq@gmail.com>
2018-09-08 12:42:13 +0800

    新增:各个get方法

--- life_game/control.py
+++ life_game/control.py
@@ -19,6 +19,10 @@ class BaseConrol(type):
 
 
 class Control(object, metaclass=BaseConrol):
+    """
+    以 `get` 开头的方法, 控制整个游戏过程中的各个属性,
+    自定义需要的属性, 需要重载这些方法.
+    """
     def __init__(self):
         self.update_cells = True
         self.paint_nums = 0
@@ -51,9 +55,30 @@ class Control(object, metaclass=BaseConrol):
     @property
     def config(self):
         return self.game.config
+    
+    def get_sleep_time(self):
+        return self.game.sleep_time
+    
+    def get_cell_color(self):
+        return self.game.cell_color
+    
+    def get_cell_size(self):
+        return self.game.cell_size
+    
+    def get_canvas_margin_left(self):
+        return self.game.canvas_margin_left
+    
+    def get_canvas_margin_top(self):
+        return self.game.canvas_margin_top
 
     def get_cell_position(self, x, y):
-        return self.game.get_cell_position(x, y)
+        """重载获取位置,细胞大小函数
+        使用该对象的方法来得到图像的位置属性,大小属性
+        """
+        return (x*self.get_cell_size()+self.get_canvas_margin_left(),
+        y*self.get_cell_size()+self.get_canvas_margin_top(),
+        (x+1)*self.get_cell_size()+self.get_canvas_margin_left(),
+        (y+1)*self.get_cell_size()+self.get_canvas_margin_top())
 
     def get_cells(self):
         for cell in self.game.get_cells():
@@ -91,6 +116,9 @@ class Control(object, metaclass=BaseConrol):
             self.after_paint()
 
     def before_paint(self):
+        """在每次画图前将以存在的图删去
+        保证每一次画图都是将全部活细胞重新绘制
+        """
         self.mapping.generate_next()
         for cell in self.get_cells():
             if cell.lived and cell.shape_obj:
@@ -98,6 +126,9 @@ class Control(object, metaclass=BaseConrol):
                 cell.shape_obj = None
 
     def paint(self):
+        """重载画图函数
+        使用该对象的方法来得到图像的属性
+        """
         for cell in self.get_cells():
             if cell.lived and not cell.shape_obj:
                 cell.shape_obj = self.canvas.create_rectangle(*self.get_cell_position(cell.x, cell.y),
@@ -109,12 +140,6 @@ class Control(object, metaclass=BaseConrol):
 
     def after_paint(self):
         self.paint_nums += 1
-
-    def get_sleep_time(self):
-        return self.game.sleep_time
-    
-    def get_cell_color(self):
-        return self.game.cell_color
 
     def after_control(self):
         self.canvas.after(self.get_sleep_time(), self.next_control_func)
